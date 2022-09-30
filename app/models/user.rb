da753dc29054c21b71f57152128ac9710b715264
chrissiku <chrissiku5@gmail.com>
2022-09-30 23:31:13 +0200

    Fix linters

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :confirmable, :lockable, :trackable, 
+         :confirmable, :lockable, :trackable,
          :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
 
   has_many :posts, foreign_key: 'user_id'
